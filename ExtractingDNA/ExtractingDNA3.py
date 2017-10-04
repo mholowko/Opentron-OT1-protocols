1b09a9e6e3e8f348f410e83ba9c1d6c17d65004d
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-04 21:49:34 +0800

    Add files via upload

---
+++ ExtractingDNA/ExtractingDNA3.py
@@ -0,0 +1,25 @@
+from opentrons import robot, instruments, containers
+
+source_plate = containers.load('trough-12row', 'B2', 'source_plate')
+plate2 = containers.load('96-PCR-flat', 'C2', 'plate2')
+elute_plate = containers.load('96-PCR-flat', 'C1', 'elute_plate')
+mag_deck= instruments.Magbead(name='mag_deck')
+m200rack2 = containers.load('tiprack-200ul', 'A2', 'm200-rack2')
+
+trash2 = containers.load('point', 'D2', 'trash2')
+
+m200 = instruments.Pipette(
+	name = "m200",
+	trash_container = trash2,
+	tip_racks = [m200rack2],
+	min_volume = 10,
+	max_volume = 200,
+	axis = "a",
+	channels = 8)
+
+#for 2 times, wash with 200ul ethanol, incubate for 30s, take out ethanol and throw.
+for row in plate2.rows():
+	m200.transfer(200, source_plate('A3'), row, new_tips='always', trash=False)
+
+for c in robot.commands():
+	print(c)
