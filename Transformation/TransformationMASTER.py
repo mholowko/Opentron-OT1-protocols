01a36620dadeb50fab75706be3eb4e805c20fb6d
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-11 21:45:56 +0800

    Update TransformationMASTER.py

--- Transformation/TransformationMASTER.py
+++ Transformation/TransformationMASTER.py
@@ -1,2 +1,9 @@
+from opentrons import robot, containers, instruments
+from opentrons.util import environment
+
+robot.connect(robot.get_serial_ports_list()[0])
+
+environment.refresh()
+
 exec(open('Transformation1.py').read())
 exec(open('Transformation2.py').read())
