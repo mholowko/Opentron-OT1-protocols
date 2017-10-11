56443a78f4f2c08940e0580ed86861ef5ced29eb
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-11 21:45:11 +0800

    Update ExtractingDNA_MASTER.py

--- ExtractingDNA/ExtractingDNA_MASTER.py
+++ ExtractingDNA/ExtractingDNA_MASTER.py
@@ -1,3 +1,10 @@
+from opentrons import robot, containers, instruments
+from opentrons.util import environment
+
+robot.connect(robot.get_serial_ports_list()[0])
+
+environment.refresh()
+
 exec(open('ExtractingDNA1.py').read())
 exec(open('ExtractingDNA2.py').read())
 exec(open('ExtractingDNA3.py').read())
