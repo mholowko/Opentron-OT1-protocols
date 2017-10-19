3a0761bda922000003e9b34acc6c9d8eb9f5c3ae
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-19 23:50:08 +0800

    Update ExtractingDNA3.py

--- ExtractingDNA/ExtractingDNA3.py
+++ ExtractingDNA/ExtractingDNA3.py
@@ -5,21 +5,28 @@ plate2 = containers.load('96-PCR-flat', 'C2', 'plate2')
 elute_plate = containers.load('96-PCR-flat', 'C1', 'elute_plate')
 mag_deck= instruments.Magbead(name='mag_deck')
 m200rack2 = containers.load('tiprack-200ul', 'A2', 'm200-rack2')
+m200rack3 = containers.load('tiprack-200ul', 'A1', 'm200-rack3')
 
 trash2 = containers.load('point', 'D2', 'trash2')
 
 m200 = instruments.Pipette(
 	name = "m200",
 	trash_container = trash2,
-	tip_racks = [m200rack2],
+	tip_racks = [m200rack2,m200rack3],
 	min_volume = 10,
 	max_volume = 200,
 	axis = "a",
 	channels = 8)
 
 #for 2 times, wash with 200ul ethanol, incubate for 30s, take out ethanol and throw.
+m200.delay(30)
+print('2nd Wash')
 for row in plate2.rows():
 	m200.transfer(200, source_plate('A3'), row, new_tips='always', trash=False)
+m200.delay(30)
+for row in plate2.rows():
+	m200.transfer(200, row, trash2, new_tips='always', trash=True)
+m200.delay(30)
+mag_deck.disengage()
 
-for c in robot.commands():
-	print(c)
+robot.run()
