4b0c80b6a20343e03a47b27fa3fc67a5dd0bd39e
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-19 23:48:24 +0800

    Update ExtractingDNA_MASTER.py

--- ExtractingDNA/ExtractingDNA_MASTER.py
+++ ExtractingDNA/ExtractingDNA_MASTER.py
@@ -1,15 +1,28 @@
 from opentrons import robot, containers, instruments
 from opentrons.util import environment
+import os
 
-robot.connect(robot.get_serial_ports_list()[0])
-
+robot.connect('/dev/ttyACM0') 
 environment.refresh()
+print(environemnt.get_path('CALIBRATIONS_FILE'))
 
+robot.home()
+
+print('There are 4 parts of Transformation Protocol')
 exec(open('ExtractingDNA1.py').read())
+print('Part 1 of Transformation is completed.')
+input('Press enter to continue...')
+robot.reset()
+robot.home()
 exec(open('ExtractingDNA2.py').read())
+print('Part 2 of Transformation is completed.')
+input('Press enter to continue...')
+robot.reset()
+robot.home()
 exec(open('ExtractingDNA3.py').read())
+print('Part 3 of Transformation is completed.')
+input('Press enter to continue...')
+robot.reset()
+robot.home()
 exec(open('ExtractingDNA4.py').read())
-exec(open('ExtractingDNA5.py').read())
-exec(open('ExtractingDNA6.py').read())
-exec(open('ExtractingDNA7.py').read())
-exec(open('ExtractingDNA8.py').read())
+print('Part 4 of Transformation is completed.')
