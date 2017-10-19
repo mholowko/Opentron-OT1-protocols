b0e0d6a353ec0d16b54d024f40978167cd74e5a6
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-19 23:49:17 +0800

    Update ExtractingDNA1.py

--- ExtractingDNA/ExtractingDNA1.py
+++ ExtractingDNA/ExtractingDNA1.py
@@ -5,13 +5,14 @@ plate2 = containers.load('96-PCR-flat', 'C2', 'plate2')
 elute_plate = containers.load('96-PCR-flat', 'C1', 'elute_plate')
 mag_deck= instruments.Magbead(name='mag_deck')
 m200rack2 = containers.load('tiprack-200ul', 'A2', 'm200-rack2')
+m200rack3 = containers.load('tiprack-200ul', 'A1', 'm200-rack3')
 
 trash2 = containers.load('point', 'D2', 'trash2')
 
 m200 = instruments.Pipette(
 	name = "m200",
 	trash_container = trash2,
-	tip_racks = [m200rack2],
+	tip_racks = [m200rack2, m200rack3],
 	min_volume = 10,
 	max_volume = 200,
 	axis = "a",
@@ -21,6 +22,12 @@ m200 = instruments.Pipette(
 #addition of AMPure XP -> total volume in each well = 56ul
 for row in plate2.rows():
 	m200.transfer(36, source_plate('A1'), row, mix_after=(10,40), new_tip='always', trash=True)
+	
+#separate beads for 2min, leave behind 5~6ul supernatant
+print('Separating beads for 2min')
+mag_deck.engage()
+m200.delay(120)
+for row in plate2.rows():
+	m200.transfer(50, row, trash2, new_tip='always', trash=True)
 
-for c in robot.commands():
-	print(c)
+robot.run()
