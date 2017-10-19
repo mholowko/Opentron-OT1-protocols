98ab4bbd706ba188200524a9c18e86aee283f03f
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-19 22:43:26 +0800

    Update TransformationMASTER.py

--- Transformation/TransformationMASTER.py
+++ Transformation/TransformationMASTER.py
@@ -1,9 +1,14 @@
 from opentrons import robot, containers, instruments
 from opentrons.util import environment
+import os
 
-robot.connect(robot.get_serial_ports_list()[0])
-
+robot.connect('/dev/ttyACM0') 
 environment.refresh()
+print(environemnt.get_path('CALIBRATIONS_FILE'))
 
+robot.home()
 exec(open('Transformation1.py').read())
+input('Press enter to continue...')
+robot.reset()
+robot.home()
 exec(open('Transformation2.py').read())
