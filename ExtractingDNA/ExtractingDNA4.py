d0bd5ed06db15770dad3c3aa71028fe96bfe9643
Yvonnefoo <31839833+Yvonnefoo@users.noreply.github.com>
2017-10-19 23:51:04 +0800

    Update ExtractingDNA4.py

--- ExtractingDNA/ExtractingDNA4.py
+++ ExtractingDNA/ExtractingDNA4.py
@@ -5,22 +5,34 @@ plate2 = containers.load('96-PCR-flat', 'C2', 'plate2')
 elute_plate = containers.load('96-PCR-flat', 'C1', 'elute_plate')
 mag_deck= instruments.Magbead(name='mag_deck')
 m200rack2 = containers.load('tiprack-200ul', 'A2', 'm200-rack2')
+m200rack3 = containers.load('tiprack-200ul', 'A1', 'm200-rack3')
 
 trash2 = containers.load('point', 'D2', 'trash2')
 
 m200 = instruments.Pipette(
 	name = "m200",
 	trash_container = trash2,
-	tip_racks = [m200rack2],
+	tip_racks = [m200rack2, m200rack3],
 	min_volume = 10,
 	max_volume = 200,
 	axis = "a",
 	channels = 8)
 
-#for 2 times, wash with 200ul ethanol, incubate for 30s, take out ethanol and throw.
-m200.delay(30)
-for row in plate2.rows():
-	m200.transfer(200, row, trash2, new_tips='always', trash=False)
+#addition of 40ul elution buffer
+print('Adding Elution Buffer')
+for row in plate2.rows:
+	m200.transfer(40, source_plate('A5'), row, mix_after=(10, 35), new_tip='always', trash=False)
+m200.delay(120)
+#separate magnetic beads from mixture
+print('Separating Magnetic Beads')
+mag_deck.engage()
+m200.delay(60)
+i=0
+for row in elute_plate.rows():
+	m200.transfer(38, plate2.rows(i), row, new_tip='always', trash=True)
+	i += 1
 
 for c in robot.commands():
 	print(c)
+
+robot.run()
